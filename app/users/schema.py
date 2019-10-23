c09606a2800efed7d173d9eb0dce75cc08e4f169
Ajay Yadav <ajay.yadav@buddy4study.com>
2019-10-24 02:36:13 +0530

    Added Users and Tracks Graph

---
+++ app/users/schema.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+import graphene
+from graphene_django import DjangoObjectType
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+
+class Query(graphene.ObjectType):
+    user = graphene.Field(UserType, email=graphene.String(required=True))
+
+    def resolve_user(self, info, email):
+        return get_user_model().objects.get(email=email)
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, **kwargs):
+        user = get_user_model()(
+            username=kwargs.get('username'),
+            email=kwargs.get('email')
+        )
+        user.set_password(kwargs.get('password'))
+        user.save()
+        return CreateUser(user=user)
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
